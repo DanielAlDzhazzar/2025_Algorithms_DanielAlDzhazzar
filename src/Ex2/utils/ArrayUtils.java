b3dd6dd92055945d7fc21dcfb132316874f05c71
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2025-09-25 06:04:15 +0100

    Ex2 1.1

---
+++ src/Ex2/utils/ArrayUtils.java
@@ -0,0 +1,25 @@
+package Ex2.utils;
+
+public class ArrayUtils
+{
+    /**
+     * Displays all elements in an array with their position.
+     *
+     * @param array
+     **/
+    public static void displayArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println("Element at index " + i + ": " + array[i]);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] array = {5, 10, 15, 20, 25};
+
+        System.out.println("Displaying array:");
+        displayArray(array);
+    }
+}
