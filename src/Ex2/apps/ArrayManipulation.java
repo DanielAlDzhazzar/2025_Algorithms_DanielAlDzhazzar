11c268638d60ae8a63efdedad42820e1a90406a8
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2025-09-29 09:53:48 +0100

    feat: add last element finder to ArrayManipulation

--- src/Ex2/apps/ArrayManipulation.java
+++ src/Ex2/apps/ArrayManipulation.java
@@ -1,10 +1,13 @@
 package Ex2.apps;
 
+import java.util.Scanner;
 import Ex2.utils.ArrayUtils;
 import Ex1.InputUtility;
 
 public class ArrayManipulation {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         int[] grades = new int[10];
 
         System.out.println("Enter 10 grades:");
@@ -21,6 +24,18 @@ public class ArrayManipulation {
 
         int highest = ArrayUtils.findMax(grades);
         System.out.println("\nHighest: " + highest);
+
+        String[] text = new String[10];
+
+        System.out.println("Enter 10 text:");
+
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println("Text " + (i + 1) + ":");
+            text[i] = input.nextLine();
+        }
+
+        String last = ArrayUtils.findMax(text);
+        System.out.println("Last element: " + last);
     }
 }
 
