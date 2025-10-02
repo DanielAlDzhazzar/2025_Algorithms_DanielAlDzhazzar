106e86a18fdc0d53889e4918019c34cb28f9a542
Daniel Al-Dzhazzar <danaldgazzar@gmail.com>
2025-10-02 12:24:54 +0100

    feat: add isIdentical(int[] arrayA, int[] arrayB) to compare 2 arrays

--- src/Main/Ex2/utils/NumberUtils.java
+++ src/Main/Ex2/utils/NumberUtils.java
@@ -1,5 +1,7 @@
 package Main.Ex2.utils;
 
+import java.util.Arrays;
+
 public class NumberUtils {
 
     /**
@@ -66,13 +68,21 @@ public class NumberUtils {
         }
     }
 
+    public static boolean isIdentical(int[] arrayA, int[] arrayB)
+    {
+        return Arrays.equals(arrayA, arrayB);
+    }
+
     public static void main(String[] args)
     {
         int num = 5;
         int[] arrayI = {5, 5, 5, 10, 10, 15, 15, 15, 15, 20, 20, 25};
+        int[] arrayII = {5, 5, 5, 10, 10, 15, 15, 15, 15, 20, 20, 25};
 
         System.out.println("First position of number 10: " + getPos(arrayI, num));
 
         System.out.println("Last position of number 10: " + getLastPos(arrayI, num));
+
+        System.out.println("Is Array A equal to Array B? " + isIdentical(arrayI, arrayII));
     }
 }
